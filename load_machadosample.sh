16490a39a579890ae63cec112a1f9dc8dc88bfd2
adhemar <adhemar.zerlotini@embrapa.br>
2023-03-10 10:32:50 -0300

    updates compose to use the oficial elasticsearch image 7.17.9

--- load_machadosample.sh
+++ load_machadosample.sh
@@ -4,6 +4,6 @@ docker-compose exec machado python manage.py load_sequence_ontology --file data/
 docker-compose exec machado python manage.py insert_organism --genus Arabidopsis --species thaliana
 docker-compose exec machado python manage.py load_fasta --file data/sample/genomes/Athaliana_167_TAIR9.chrM.fa --soterm chromosome --organism 'Arabidopsis thaliana'
 docker-compose exec machado python manage.py load_gff --file data/sample/genomes/Athaliana_167_TAIR10.chrM.gff3.gz --organism 'Arabidopsis thaliana' --cpu 2
-docker-compose exec machado python manage.py load_feature_annotation --file data/sample/genomes/Athaliana_167_TAIR10.defline.ChrM.txt --soterm mRNA --cvterm display
-docker-compose exec machado python manage.py load_feature_annotation --file data/sample/genomes/Athaliana_167_TAIR10.defline.ChrM.txt --soterm polypeptide --cvterm display
+docker-compose exec machado python manage.py load_feature_annotation --file data/sample/genomes/Athaliana_167_TAIR10.defline.ChrM.txt --soterm mRNA --cvterm display --organism 'Arabidopsis thaliana'
+docker-compose exec machado python manage.py load_feature_annotation --file data/sample/genomes/Athaliana_167_TAIR10.defline.ChrM.txt --soterm polypeptide --cvterm display --organism 'Arabidopsis thaliana'
 docker-compose exec machado python manage.py rebuild_index
